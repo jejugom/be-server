6b546a7f50237337519ac20f176879fc13183667
ParkPark8 <tqtq9898@gmail.com>
2025-07-25 09:41:37 +0900

    chore:간단한 수정 및 주석 추가

--- src/main/java/org/scoula/auth/controller/AuthController.java
+++ src/main/java/org/scoula/auth/controller/AuthController.java
@@ -7,6 +7,9 @@ import org.scoula.auth.dto.LoginResponseDto;
 import org.scoula.auth.dto.RefreshTokenRequestDto;
 import org.scoula.auth.dto.TokenRefreshResponseDto;
 import org.scoula.auth.service.KakaoAuthService;
+import org.scoula.user.domain.UserVo;
+import org.scoula.user.dto.UserInfoDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,16 +36,34 @@ public class AuthController {
 		return ResponseEntity.ok(loginResponse);
 	}
 
-	// ✅ 반환 타입을 ResponseEntity<LoginResponseDto>로 수정
+	/***
+	 * 개발 단계에서는 callBack호출 시 바로 화면에 LoginDTO 띄워주게끔
+	 * 이후 프론트 연동 시 /kakao/callback 매핑 주석/삭제 처리
+	 * 현 단계에서는 /auth/kakao 로 카카오 인가코드 삽입 시 에러 나는것이 정상.
+	 */
+
 	@PostMapping("/kakao")
-	public ResponseEntity<LoginResponseDto> kakaoLogin(@RequestBody Map<String, String> body) {
+	public ResponseEntity<?> kakaoLogin(@RequestBody Map<String, String> body) {
 		String code = body.get("code");
+		try {
+			// 1. 카카오 로그인 처리 (토큰 요청 + 사용자 조회 + DB 저장)
+			// 서비스는 LoginResponseDto를 반환
+			LoginResponseDto loginResponse = kakaoAuthService.processKakaoLogin(code);
+			// 2. JWT 발급
+			return ResponseEntity.ok(loginResponse);
 
-		// 서비스는 LoginResponseDto를 반환
-		LoginResponseDto loginResponse = kakaoAuthService.processKakaoLogin(code);
+		} catch (HttpClientErrorException e) {
+			if (e.getStatusCode() == HttpStatus.BAD_REQUEST &&
+				e.getResponseBodyAsString().contains("KOE320")) {
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+					.body(Map.of("error","유효하지 않은 인가코드입니다."));
 
-		// 수정된 반환 타입과 일치하므로 에러가 발생하지 않음
-		return ResponseEntity.ok(loginResponse);
+			}
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(Map.of("error","카카오 서버와 연결되지 않는 상태입니다. "));
+
+		}
+
 	}
 
 	/**
