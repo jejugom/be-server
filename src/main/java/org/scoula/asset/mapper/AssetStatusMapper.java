534f1c6d62df2492862ea5648660045aa58b8006
ParkPark8 <tqtq9898@gmail.com>
2025-07-29 13:43:26 +0900

    NHDR-155 feat: 나의 자산 조회,수정,삭제 기능 구현

--- src/main/java/org/scoula/asset/mapper/AssetStatusMapper.java
+++ src/main/java/org/scoula/asset/mapper/AssetStatusMapper.java
@@ -2,10 +2,12 @@ package org.scoula.asset.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.scoula.asset.domain.AssetStatusVo;
 
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface AssetStatusMapper {
 	List<AssetStatusVo> findAssetStatusByEmail(String email);
 
