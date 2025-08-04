c76011657ce3908c1df306c4638a8cbed0b750f4
winA <92146550+a40418a@users.noreply.github.com>
2025-08-04 21:09:09 +0900

    feat: 뉴스 크롤링 코드 작성

---
+++ src/main/java/org/scoula/news/scheduler/NewsScheduler.java
@@ -0,0 +1,20 @@
+package org.scoula.news.scheduler;
+
+import org.scoula.news.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class NewsScheduler {
+
+  @Autowired
+  private NewsService newsService;
+
+  @Scheduled(cron = "0 0 12 * * *")
+  public void scheduleNewsCrawling() {
+    newsService.crawlAndSaveNews();
+  }
+}
